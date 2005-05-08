05620518502f339fb5d3a9a88768fcef8830fa72
Georgi D. Sotirov <gdsotirov@dir.bg>
2005-05-08 19:57:18 +0000

    Use png icon for valid XHTML

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
 </td></tr></table></form>
 <!-- Valid XHTML 1.0 Transitional, Valid CSS //-->
 <p align="center"><a href="http://validator.w3.org/check/referer">
-<img border="0" src="valid-xhtml10.gif" alt="Valid XHTML 1.0!"
+<img border="0" src="valid-xhtml.png" alt="Valid XHTML 1.0!"
 height="31" width="88" /></a>
 <a class="right" href="http://jigsaw.w3.org/css-validator/check/referer">
 <img alt="Valid CSS!" border="0" height="31" src="valid-css.png" width="88" />
